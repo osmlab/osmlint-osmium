26cb4e517464b68205262607e2423b15e0ec0fcb
Rub21 <ruben@mapbox.com>
2017-09-06 16:21:02 -0500

    rename

--- util/index.js
+++ util/index.js
@@ -2,14 +2,13 @@
 
 /* eslint-disable global-require */
 module.exports = {
-	'nodeFeature': require('./nodeFeature'),
-	'wayFeature': require('./wayFeature'),
+	'mergeNodeRelationFeature': require('./mergeNodeRelationFeature'),
+	'mergeWayRelationFeature': require('./mergeWayRelationFeature'),
 	'relationFeature': require('./relationFeature'),
 	'prefixTag': require('./prefixTag'),
 	'checkTags': require('./checkTags'),
 	'sortRoles': require('./sortRoles'),
 	'isContinuous': require('./isContinuous'),
-		'simpleRole': require('./simpleRole')
-
+	'simpleRole': require('./simpleRole')
 };
 /* eslint-enable global-require */
---
+++ util/mergeWayRelationFeature.js
@@ -0,0 +1,26 @@
+'use strict';
+var _ = require('underscore');
+var prefix = require('./prefixTag');
+
+/**
+ * @param  {object} way object
+ * @param  {object} realtions tag
+ * @return {object} geojson feature, it include the current tags and the relation tag
+ */
+module.exports = function(way, relation, rol) {
+  // console.log(relation.tags);
+  var properties = _.extend({
+    '@id': way.id,
+    '@version': way.version,
+    '@changeset': way.changeset,
+    '@uid': way.uid,
+    '@user': way.user,
+    '@idrel': relation.props['@id']
+  }, way.tags(), prefix('rProp_', relation.props), prefix('rTag_', relation.tags), prefix('rRol_', rol));
+  var feature = {
+    type: 'Feature',
+    properties: properties,
+    geometry: way.geojson()
+  };
+  return feature;
+};
