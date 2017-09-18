4976a1f46ff8411e0d59d3b20b8945ee513900af
Rub21 <ruben@mapbox.com>
2017-09-18 17:47:33 -0500

    fix features for nodes and ways

--- src/util/index.js
+++ src/util/index.js
@@ -2,13 +2,15 @@
 
 /* eslint-disable global-require */
 module.exports = {
-  'mergeNodeRelationFeature': require('./mergeNodeRelationFeature'),
-  'mergeWayRelationFeature': require('./mergeWayRelationFeature'),
-  'relationFeature': require('./relationFeature'),
-  'prefixTag': require('./prefixTag'),
-  'checkTags': require('./checkTags'),
-  'sortRoles': require('./sortRoles'),
-  'isContinuous': require('./isContinuous'),
-  'simpleRole': require('./simpleRole')
-}
-/* eslint-enable global-require */
+		'mergeNodeRelationFeature': require('./mergeNodeRelationFeature'),
+		'mergeWayRelationFeature': require('./mergeWayRelationFeature'),
+		'relationFeature': require('./relationFeature'),
+		'prefixTag': require('./prefixTag'),
+		'checkTags': require('./checkTags'),
+		'sortRoles': require('./sortRoles'),
+		'isContinuous': require('./isContinuous'),
+		'simpleRole': require('./simpleRole'),
+		'wayFeature': require('./wayFeature'),
+		'nodeFeature': require('./nodeFeature')
+	}
+	/* eslint-enable global-require */
--- src/util/nodeFeature.js
+++ src/util/nodeFeature.js
@@ -2,20 +2,16 @@
 var _ = require('underscore')
 var prefix = require('./prefixTag')
 
-/**
- * @param  {object} node object
- * @param  {object} realtions tag
- * @return {object} geojson feature, it include the current tags and the relation tag
- */
-module.exports = function (node, relation, rol) {
+
+module.exports = function(node) {
   var properties = _.extend({
     '@id': node.id,
     '@version': node.version,
     '@changeset': node.changeset,
     '@uid': node.uid,
     '@user': node.user,
-    '@idrel': relation.props['@id']
-  }, node.tags(), prefix('rProp_', relation.props), prefix('rTag_', relation.tags), prefix('rRol_', rol))
+    '@timestamp': node.timestamp_seconds_since_epoch
+  }, node.tags())
   var feature = {
     type: 'Feature',
     properties: properties,
